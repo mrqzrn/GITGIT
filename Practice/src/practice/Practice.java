6dfc635c1911540aba9243d8835fad5e189abe57
mrqzrn <ron.marquez139@gmail.com>
2022-08-27 16:49:06 +0800

    Initial Commit

--- Practice/src/practice/Practice.java
+++ Practice/src/practice/Practice.java
@@ -16,6 +16,7 @@ public class Practice {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("UPDATE GIT");
     }
     
 }
